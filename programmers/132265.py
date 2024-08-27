014c0416d9e5330ffcd0b672cf9a7ad122e24101
Hyowon Jeon <68677647+hyowon612@users.noreply.github.com>
2024-08-27 09:52:30 +0900

    [프로그래머스] 132265 롤케이크 자르기

---
+++ programmers/132265.py
@@ -0,0 +1,26 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/132265
+
+def solution(topping):
+    answer = 0
+    left = set()
+    right = dict()
+
+    for i in topping:
+        if i in right:
+            right[i] += 1
+        else:
+            right[i] = 1
+
+    for i in topping:
+        right[i] -= 1
+        left.add(i)
+
+        if right[i] == 0:
+            right.pop(i)
+
+        if len(left) == len(right):
+            answer += 1
+        elif len(left) > len(right):
+            break
+
+    return answer
